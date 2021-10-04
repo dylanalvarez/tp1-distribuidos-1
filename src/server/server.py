c5dee23710f7a80795b874d3bfb2601515a099d4
Dylan Alvarez <dylan.alvarez@greencodesoftware.com>
2021-10-04 18:14:33 -0300

    Handle server errors gracefully

--- src/server/server.py
+++ src/server/server.py
@@ -1,22 +1,31 @@
+import _queue
 import logging
-import queue
 import signal
 import socket
-from multiprocessing import Process, Queue, Manager
+from multiprocessing import Process, Queue, Manager, Value
 
 from src.server.invalid_request import InvalidRequest
 
 
-def handle_client_requests(index, request_queue, generate_response, open_filenames, available_process_indices):
-    while True:
-        msg, client_sock = request_queue.get()
+def do_nothing(_, __):
+    pass
+
+
+def handle_client_requests(index, request_queue, generate_response, open_filenames, available_process_indices, must_exit):
+    signal.signal(signal.SIGTERM, do_nothing)
+    signal.signal(signal.SIGINT, do_nothing)
+    while not must_exit.value:
         try:
-            client_sock.send(generate_response(msg[:-1], client_sock, open_filenames).encode('utf-8'))
-        except InvalidRequest:
-            client_sock.send(b'{"error": "invalid request"}\n')
-        finally:
-            available_process_indices.put(index)
-        client_sock.close()
+            msg, client_sock = request_queue.get(timeout=1)
+            try:
+                client_sock.send(generate_response(msg[:-1], client_sock, open_filenames).encode('utf-8'))
+            except InvalidRequest:
+                client_sock.send(b'{"error": "invalid request"}\n')
+            finally:
+                available_process_indices.put(index)
+            client_sock.close()
+        except _queue.Empty:
+            pass
 
 
 class Server:
@@ -26,7 +35,7 @@ class Server:
         self._server_socket.settimeout(1.0)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
-        self.must_exit = False
+        self.must_exit = Value('i', False)
         self.generate_response = generate_response
         self.processes = []
         self.process_queues = []
@@ -37,7 +46,7 @@ class Server:
             self.available_process_indices.put(index)
             request_queue = Queue()
             self.process_queues.append(request_queue)
-            process = Process(target=handle_client_requests, args=(index, request_queue, generate_response, self.open_filenames, self.available_process_indices))
+            process = Process(target=handle_client_requests, args=(index, request_queue, generate_response, self.open_filenames, self.available_process_indices, self.must_exit))
             process.start()
             self.processes.append(process)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
@@ -45,7 +54,7 @@ class Server:
 
     def exit_gracefully(self, _, __):
         print("Exiting gracefully")
-        self.must_exit = True
+        self.must_exit.value = True
 
     def run(self):
         """
@@ -55,13 +64,13 @@ class Server:
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
-        while not self.must_exit:
+        while not self.must_exit.value:
             client_sock = self.__accept_new_connection()
             if client_sock:
                 self.__handle_client_connection(client_sock)
         self._server_socket.close()
         for process in self.processes:
-            process.terminate()
+            process.join()
         print("Exited gracefully!")
 
     def __handle_client_connection(self, client_sock):
@@ -74,14 +83,14 @@ class Server:
         client_sock.settimeout(10)
         try:
             msg = ''
-            while not (msg.endswith('\n') or self.must_exit):
+            while not (msg.endswith('\n') or bool(self.must_exit.value)):
                 msg += client_sock.recv(10).decode('utf-8')
             if not msg.endswith('\n'):
                 return
             try:
                 index = self.available_process_indices.get_nowait()
                 self.process_queues[index].put((msg, client_sock))
-            except queue.Full:
+            except _queue.Full:
                 client_sock.send(b'{"error": "service unavailable"}\n')
                 client_sock.close()
         except OSError:
@@ -99,7 +108,7 @@ class Server:
         logging.info("Proceed to accept new connections")
         c = None
         address = None
-        while not (c or self.must_exit):
+        while not (c or bool(self.must_exit.value)):
             try:
                 c, address = self._server_socket.accept()
             except socket.timeout as e:
